e7161f6b061654bc76f24947a9587cc13d6e1176
Honza <horacek.94@gmail.com>
2021-04-01 05:10:33 +0200

    Profile fragment zmena jmena implementovana

--- app/src/main/java/tech/janhoracek/debtdragon/profile/ui/ChangeNameFragment.kt
+++ app/src/main/java/tech/janhoracek/debtdragon/profile/ui/ChangeNameFragment.kt
@@ -1,13 +1,26 @@
 package tech.janhoracek.debtdragon.profile.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.flow.onEach
+import tech.janhoracek.debtdragon.MainActivity
 import tech.janhoracek.debtdragon.R
+import tech.janhoracek.debtdragon.databinding.FragmentChangeNameBinding
+import tech.janhoracek.debtdragon.profile.viewmodels.ChangeNameViewModel
+import tech.janhoracek.debtdragon.utility.BaseFragment
+import tech.janhoracek.debtdragon.utility.observeInLifecycle
 
-class ChangeNameFragment : Fragment() {
+class ChangeNameFragment : BaseFragment() {
+    override var bottomNavigationViewVisibility = View.GONE
+    private lateinit var binding: FragmentChangeNameBinding
+    val viewModel by viewModels<ChangeNameViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +31,28 @@ class ChangeNameFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_change_name, container, false)
+        requireActivity().window.statusBarColor = Color.parseColor("#FFFFFF")
+        binding = FragmentChangeNameBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewmodel = viewModel
+
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewmodel!!.eventsFlow
+            .onEach {
+                when(it) {
+                    ChangeNameViewModel.Event.NameChanged -> {
+                        (activity as MainActivity).hideLoading()
+                        Toast.makeText(requireContext(), "Jméno změněno", Toast.LENGTH_LONG).show()
+                        findNavController().navigateUp()
+                    }
+                    ChangeNameViewModel.Event.ShowLoading -> {(activity as MainActivity).showLoading()}
+                }
+            }.observeInLifecycle(viewLifecycleOwner)
     }
 
 }
---
+++ app/src/main/java/tech/janhoracek/debtdragon/profile/viewmodels/ChangeNameViewModel.kt
@@ -0,0 +1,52 @@
+package tech.janhoracek.debtdragon.profile.viewmodels
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import tech.janhoracek.debtdragon.utility.BaseViewModel
+import tech.janhoracek.debtdragon.utility.Constants
+
+class ChangeNameViewModel: BaseViewModel() {
+
+    val userName = MutableLiveData<String>()
+
+    private val _nameError = MutableLiveData<String>("")
+    val nameError: LiveData<String> get() = _nameError
+
+    sealed class Event {
+        object NameChanged : Event()
+        object ShowLoading : Event()
+    }
+
+    private val eventChannel = Channel<Event>(Channel.BUFFERED)
+    val eventsFlow = eventChannel.receiveAsFlow()
+
+    init {
+        GlobalScope.launch(IO) {
+            db.collection(Constants.DATABASE_USERS).document(auth.currentUser.uid).get().addOnSuccessListener { document ->
+                userName.postValue(document[Constants.DATABASE_USER_NAME].toString())
+            }
+        }
+    }
+
+    fun changeName() {
+        if(!userName.value.isNullOrEmpty()) {
+            _nameError.value = ""
+            GlobalScope.launch(IO) {
+                eventChannel.send(Event.ShowLoading)
+                db.collection(Constants.DATABASE_USERS).document(auth.currentUser.uid).update(Constants.DATABASE_USER_NAME, userName.value).await()
+                eventChannel.send(Event.NameChanged)
+            }
+            Log.d("CAJ", "Muzeme menit jmena")
+        } else {
+            _nameError.value = "Jméno nemůže být prázdné"
+        }
+    }
+
+}
